5a2c8affdd1a18a8f90a63b737951cb5f60282d1
Venkatesh <76658782+venkiganesh@users.noreply.github.com>
2022-03-17 07:59:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+function heart() {
+  var x = document.getElementById('heart');
+  var y = document.getElementById('liked');
+
+  if(x.style.color=="red"){
+    x.style.color="black";
+    y.style.opacity="0";
+
+  }
+  else{
+    x.style.color="red";
+    y.style.animation="liked 0.2s forwards linear";
+
+  }
+
+}
+function heart2() {
+  var y = document.getElementById('heart2');
+  var x = document.getElementById('liked2');
+
+  if(y.style.color=="red"){
+    y.style.color="black";
+    x.style.opacity="0";
+
+  }
+  else{
+    y.style.color="red";
+    x.style.opacity="1";
+
+  }
+
+}
+function heart3() {
+  var y = document.getElementById('heart3');
+  var x = document.getElementById('liked3');
+
+  if(y.style.color=="red"){
+    y.style.color="black";
+    x.style.opacity="0";
+
+  }
+  else{
+    y.style.color="red";
+    x.style.opacity="1";
+
+  }
+
+}
+function heart4() {
+  var y = document.getElementById('heart4');
+  var x = document.getElementById('liked4');
+
+  if(y.style.color=="red"){
+    y.style.color="black";
+    x.style.opacity="0";
+
+  }
+  else{
+    y.style.color="red";
+    x.style.opacity="1";
+
+  }
+
+}
+function heart5() {
+  var y = document.getElementById('heart5');
+  var x = document.getElementById('liked5');
+
+  if(y.style.color=="red"){
+    y.style.color="black";
+    x.style.opacity="0";
+
+  }
+  else{
+    y.style.color="red";
+    x.style.opacity="1";
+
+  }
+
+}
